8c96fe789bf2a6efc6d20f3260d274a7b6dbc77e
ariful4082 <ariful4082@gmail.com>
2021-04-10 00:41:28 +0600

    old style of get constractor fuction with props for class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,19 @@
 import React, { Component } from 'react';
 
-class Child extends Component{
-    render(){
-        this.props.func(this)
-        return <h1>I am Child</h1>
-    }
-}
-
-const ChildComponent = (props)=>(
-    <div>
-        <h3>I am child Component</h3>
-        <p>I don't know what to do</p>
-        {
-            props.children
-        }
-    </div>
-
-)
 
 class App extends Component {
-    getContext(context){
-        console.log(context);
+
+    constructor(props) {
+        super(props)
+        this.count = 5;
     }
 
     render() {
-    
-        this.getContext(this)
+        console.log(this.count);
         return (
             <div className="App" >
-                <h1>Pass Fucntion As Props</h1>
-                {/* <Child func={this.getContext}/> */}
-                <ChildComponent>
-                    <h2>Hello I am from parent</h2>
-                    <h4>I am child fo Child Componet</h4>
-                </ChildComponent>
+                <h1>Why do we need state</h1>
+                <h1>Count = {this.count}</h1>
             </div>
         )
     }
