229c543baa70584e1c6f73731b5688d33196c2f6
ariful4082 <ariful4082@gmail.com>
2021-04-09 10:44:19 +0600

    create a simple profile ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
+import Profile from './Profile';
 
-const MyComponent = () => <h1>My Component</h1>
 
 class App extends Component {
 
     render() {
     
-        const obj = {
-            title: 'Test Title',
-            id: 'myh1'
-        }
-
-        const name = 'Ariful Islam'
-        const bio = (
-            <div>
-            <h3>Abdullah Al Zarif</h3>
-            <p>Son of {name} </p>
-        </div>
-        )
+       
         return (
-            <div className="App">
-                <h1 {...obj}>What is JSX?</h1>
-                <p>JSX is Awesome and it's Mean Javascript Extension</p>
-                <p>{new Date().toISOString()}</p>
-                <p>My name is {name}</p>
-                <MyComponent />
-                {bio}
+            <div >
+                <Profile/>
             </div>
         )
     }
