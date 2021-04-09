aed3b7bd166b728da0878a10c32d66dc99621f0d
ariful4082 <ariful4082@gmail.com>
2021-04-09 23:27:25 +0600

    provite props fucntion to child components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
-import Profile from './Profile';
-import Bio from './Profile/Bio';
-import MyProps from './Props';
-import Skills from './Profile/Skills';
 
+class Child extends Component{
+    render(){
+        this.props.func(this)
+        return <h1>I am Child</h1>
+    }
+}
 
 class App extends Component {
+    getContext(context){
+        console.log(context);
+    }
 
     render() {
     
-       
+        this.getContext(this)
         return (
             <div className="App" >
-                <Profile/>
-                <Bio name="test user" title="Test title"/>
-                <div style={{margin: '30px 0'}}>
-                    <h3 style={{marginTop: '30px'}}>List of Programmes</h3>
-                    <p>Mr.X</p>
-                    <Skills skillA="React" skillB="Angular" skillC="Vue"/>
-
-                    <p>Mr. Y</p>
-                    <Skills skillA="Node" skillB="Mongodb" skillC="Express"/>
-                    <MyProps name="Abdullah Al Zarif"/>
-                    <MyProps name="Aysha Mone"/>
-                    <MyProps name="Ariful Islam"/>
-                </div>
+                <h1>Pass Fucntion As Props</h1>
+                <Child func={this.getContext}/>
             </div>
         )
     }
