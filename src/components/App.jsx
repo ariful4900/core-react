5ad2e1bbe3203cf4cf6182ce2b97d1adebdf1e86
ariful4082 <ariful4082@gmail.com>
2021-04-10 00:54:18 +0600

    update react state by basic way

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0
+        }
+    }
 
-    count = 0
+    count = 0;
 
     render() {
         console.log(this.count);
         return (
             <div className="App" >
                 <h1>Why do we need state</h1>
-                <h1>Count = {this.count}</h1>
-                <button onClick={() => {this.count++; console.log('clicked', this.count);}}>Increament</button>
+                <h1>Count = {this.state.count}</h1>
+                <button onClick={() => {
+                    // this.count++; 
+                    this.setState({count: this.state.count+1})
+                    console.log('clicked', this.state.count);
+                    }}>Increament</button>
             </div>
         )
     }
