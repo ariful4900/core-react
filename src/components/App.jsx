a620696cc5464de3c07bdf8a60e64bca73a86540
ariful4082 <ariful4082@gmail.com>
2021-04-09 09:09:06 +0600

    discus how convert react in jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,24 @@ import React, { Component } from 'react';
 class App extends Component{
 
     render(){
-        return <h1>That's it is working</h1>
+        return React.createElement('div', {className: 'App'}, [
+            React.createElement('h1', null, 'What is JSX?'),
+            React.createElement('p', null, 'JSX is Awesome and it\'s Mean Javascript Extension')
+        ])
+
+        // return <div>
+        //     <h1>What is JSX?</h1>
+        // </div>
     }
 }
 export default App
 
 
-function MyFuctionalComponent(){
-    const name = 'Ariful Islam';
-    return <h1>I am functional Components</h1>
+const element = {
+    type: 'div',
+    props: {
+        className: 'test',
+        title: 'Test Me'
+    },
+    children: ['p', 'h1', 'img']
 }
-
-
-const AnotherFuctionalComponent = () => <h1>I am Another Functional Component</h1>
