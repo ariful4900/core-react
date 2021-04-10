c68aac928423f9c3e84ed56f761b48fd5644fb6e
ariful4082 <ariful4082@gmail.com>
2021-04-10 16:16:37 +0600

    update state value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            count: 0
-        }
+
+
+    state = {
+        count: 0,
+        lotOfproperties: {}
     }
 
-    count = 0;
-
     render() {
-        console.log(this.count);
         return (
             <div className="App" >
                 <h1>Why do we need state</h1>
                 <h1>Count = {this.state.count}</h1>
-                <button onClick={() => {
-                    // this.count++; 
-                    this.setState({count: this.state.count+1})
-                    console.log('clicked', this.state.count);
-                    }}>Increament</button>
-            </div>
+                <button
+                    onClick={()=>{
+                        this.setState(
+                            prev => {
+                                return {
+                                    count: prev.count + 1
+                                }
+                            },()=>{
+                                console.log('clicked', this.state.count);
+                            }
+                        )
+                    }}
+                >
+                    Incremnet
+                </button>
+            </div >
         )
     }
 }
