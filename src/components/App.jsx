afc00a5881b6fcfd5dfb1cc1a21e26aff73fee29
ariful4082 <ariful4082@gmail.com>
2021-04-10 17:12:59 +0600

    working successfully simple timer fuction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,29 +6,60 @@ class App extends Component {
 
     state = {
         count: 0,
-        lotOfproperties: {}
+
+    }
+    intervalId = null
+    incrementCount = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+    dicrementCount = () => {
+
+        if (this.state.count > 0) {
+            this.setState({ count: this.state.count - 1 })
+        }
+
+    }
+    startTimer = () => {
+        if (this.state.count > 0 && !this.intervalId) {
+            this.intervalId = setInterval(() => {
+                this.setState({ count: this.state.count - 1 }, () => {
+                    if (this.state.count === 0) {
+                        alert('Timer Finished')
+                        clearInterval(this.intervalId)
+                        this.intervalId =null
+                    }
+                })
+            }, 1000)
+        }
+    }
+
+    stopTimer = ()=>{
+        if(this.intervalId){
+            clearInterval(this.intervalId)
+            this.intervalId = null
+        }
+    }
+
+    resetTimer = ()=>{
+        this.setState({count: 0});
+        clearInterval(this.intervalId)
+        this.intervalId = null
     }
 
     render() {
         return (
             <div className="App" >
-                <h1>Why do we need state</h1>
-                <h1>Count = {this.state.count}</h1>
-                <button
-                    onClick={()=>{
-                        this.setState(
-                            prev => {
-                                return {
-                                    count: prev.count + 1
-                                }
-                            },()=>{
-                                console.log('clicked', this.state.count);
-                            }
-                        )
-                    }}
-                >
-                    Incremnet
-                </button>
+                <h1 className="heading">Simple Timer</h1>
+                <div className="container">
+                    <button onClick={this.dicrementCount}>-</button>
+                    <span>{this.state.count}</span>
+                    <button onClick={this.incrementCount}>+</button>
+                </div>
+                <div className="container">
+                    <button onClick={this.startTimer}>Start</button>
+                    <button onClick={this.resetTimer}>Reset</button>
+                    <button onClick={this.stopTimer}>Stop</button>
+                </div>
             </div >
         )
     }
