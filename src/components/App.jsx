d8a2567e16083ada98b09b1894e307d383043c9b
ariful4082 <ariful4082@gmail.com>
2021-04-10 23:11:56 +0600

    inline styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,26 @@
 import React, { Component } from 'react';
 
-
+const myHeader = {
+    color: 'blue',
+    fontWeight: 300,
+    fontFamily: 'Arial',
+    fontSize: '36px'
+}
 class App extends Component {
 
 
-    state = {
-        count: 0,
 
-    }
-    intervalId = null
-    incrementCount = () => {
-        this.setState({ count: this.state.count + 1 })
-    }
-    dicrementCount = () => {
-
-        if (this.state.count > 0) {
-            this.setState({ count: this.state.count - 1 })
-        }
-
-    }
-    startTimer = () => {
-        if (this.state.count > 0 && !this.intervalId) {
-            this.intervalId = setInterval(() => {
-                this.setState({ count: this.state.count - 1 }, () => {
-                    if (this.state.count === 0) {
-                        alert('Timer Finished')
-                        clearInterval(this.intervalId)
-                        this.intervalId =null
-                    }
-                })
-            }, 1000)
-        }
-    }
-
-    stopTimer = ()=>{
-        if(this.intervalId){
-            clearInterval(this.intervalId)
-            this.intervalId = null
-        }
-    }
-
-    resetTimer = ()=>{
-        this.setState({count: 0});
-        clearInterval(this.intervalId)
-        this.intervalId = null
-    }
 
     render() {
         return (
             <div className="App" >
-                <h1 className="heading">Simple Timer</h1>
-                <div className="container">
-                    <button onClick={this.dicrementCount}>-</button>
-                    <span>{this.state.count}</span>
-                    <button onClick={this.incrementCount}>+</button>
-                </div>
-                <div className="container">
-                    <button onClick={this.startTimer}>Start</button>
-                    <button onClick={this.resetTimer}>Reset</button>
-                    <button onClick={this.stopTimer}>Stop</button>
-                </div>
+                <h1 style={myHeader} >How to Style React App</h1>
+                <h1 style={{
+                    color: 'orange',
+                    fontWeight: 300,
+                    fontFamily: 'Arial',
+                    fontSize: '36px'
+                }}>Another Header Tag</h1>
             </div >
         )
     }
